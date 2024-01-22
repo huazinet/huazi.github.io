e4ec204294257e7881153db5724fb473ed83d42b
huazinet <1362600758@qq.com>
2024-01-22 15:29:53 +0800

    three

--- docs/.vitepress/theme/components/About.vue
+++ docs/.vitepress/theme/components/About.vue
@@ -4,16 +4,13 @@
         <div class="desc">
             <p>这是一个非常素雅的技术型分享博客，基于 vitepress 开发。</p>
 
-            <p>主要整理分享前端大牛们的开源作品和付费作品。</p>
-            <p>包括：vue3+ts开源项目、小程序开源作品、AIGC开源项目、对开发有帮助的设计源文件等</p>
-
             <p>所有作品版权归原作者所有。</p>
             <p>本站不对作品的完整性、可用性和时效性负责。</p>
         </div>
         <div class="list">
             <h4 class="h4">-- 信息 --</h4>
             <ul class="ul">
-                <li class="li"><label class="label">博主</label><span>小鱼哥</span></li>
+                <li class="li"><label class="label">博主</label><span>华子</span></li>
                 <li class="li"><label class="label">程序</label><span>Vitepress</span></li>
                 <li class="li"><label class="label">图标</label><span>remixicon</span></li>
                 <li class="li"><label class="label">音乐</label><span>来源网络</span></li>
--- docs/.vitepress/theme/genFeed.ts
+++ docs/.vitepress/theme/genFeed.ts
@@ -4,19 +4,19 @@ import { writeFileSync } from 'fs'
 import { Feed } from 'feed'
 import { createContentLoader, type SiteConfig } from 'vitepress'
 
-const baseUrl = `https://appbeebee.com`
+const baseUrl = `https://xb.pearson.live`
 
 export async function genFeed(config: SiteConfig) {
   const feed = new Feed({
-    title: 'APP比比',
-    description: '前端技术分享博客',
+    title: '华子Net',
+    description: '羊毛，技术分享博客',
     id: baseUrl,
     link: baseUrl,
     language: 'zh',
     image: `${baseUrl}/logo.png`,
     favicon: `${baseUrl}/favicon.ico`,
     copyright:
-      'Copyright © 2023-'+ new Date().getFullYear() + ' APP比比'
+      'Copyright © 2024-'+ new Date().getFullYear() + ' 华子Net'
   })
 
   const posts = await createContentLoader('posts/*/*/*.md', {
@@ -36,7 +36,7 @@ export async function genFeed(config: SiteConfig) {
       id: `${baseUrl}${url}`,
       link: `${baseUrl}${url}`,
       description: excerpt,
-      content: `<img src="${frontmatter.cover}" alt="" /><p><h3><a href="${baseUrl}${url}">文章涉及下载地址访问原站：APP比比</a></h3></p>` + html + `<p><h3 style="color:#3451b2"><a href="${baseUrl}${url}">文章涉及下载地址访问原站：APP比比</a></h3></p>`,
+      content: `<img src="${frontmatter.cover}" alt="" /><p><h3><a href="${baseUrl}${url}">文章涉及下载地址访问原站：https://xb.pearson.live</a></h3></p>` + html + `<p><h3 style="color:#3451b2"><a href="${baseUrl}${url}">文章涉及下载地址访问原站：https://xb.pearson.live</a></h3></p>`,
       image: frontmatter.cover,
       date: frontmatter.date
     })
