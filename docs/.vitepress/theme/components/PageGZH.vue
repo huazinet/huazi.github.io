6b251a9e7def64b1a45e66e3a6c4db4b856aeab3
huazinet <95842877+huazinet@users.noreply.github.com>
2024-01-25 12:09:35 +0800

    Update PageGZH.vue

--- docs/.vitepress/theme/components/PageGZH.vue
+++ docs/.vitepress/theme/components/PageGZH.vue
@@ -5,10 +5,10 @@
       <p class="title">分享羊毛技术</p>
       <p style="margin-top: 5px;">技术｜源码｜设计｜参考</p>
     </div>
-    <div><img src="https://cdnjson.com/images/2024/01/25/QRcode_SP--3.png" alt="公众号二维码" /></div>
+    <div><img src="https://cdnjson.com/images/2024/01/25/QRcode_B1.png" alt="公众号二维码" /></div>
     <div>
       <p>
-        扫码关注华子Network</p>
+        TG扫码添加华子Network</p>
       <p style="margin-top: 5px;">
         第一时间传递给你</p>
     </div>
