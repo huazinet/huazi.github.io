e593102160983877f1a0007aaf4d09f23768a1bb
huazinet <95842877+huazinet@users.noreply.github.com>
2024-07-23 13:44:26 +0800

    Update PageSlider.vue

--- docs/.vitepress/theme/components/PageSlider.vue
+++ docs/.vitepress/theme/components/PageSlider.vue
@@ -26,7 +26,7 @@
                 <div>
                     <Countdown :until="until" />
                 </div>
-                <a href="https://sharehub.club/posts/2024/04/bilibili_automation.html" class="tolink">
+                <a href="https://sharehub.club/posts/2024/04/bilibili_automation.html">
                 <span class="span">点我活右侧扫码了解</span><svg class="svf" width="15" height="15"
                         viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
